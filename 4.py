d5173a2fc7415228b82533403b348f5a81d42e5c
Vishnu-Priya0607 <119881668+Vishnu-Priya0607@users.noreply.github.com>
2023-02-13 21:40:44 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,5 @@
+list_1=['andrew','arun','athullya','athira','anand','ammu','asiya','anju']
+total=0
+for i in list_1:
+    total+=i.count('a')
+print(total)
---
+++ 6.py
@@ -0,0 +1,9 @@
+word_1=input("Enter any word :")
+
+word=list(word_1)
+word[0],word[-1]=word[-1],word[0]
+word_2="".join([str(elem) for elem in word])
+
+print(word_2)
+Footer
+© 2022 GitHub, Inc. 
